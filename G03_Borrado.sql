566349e4f56a845aea9e4a1458cf92bf54d36ba8
Tomas <tomasporcaro22@gmail.com>
2020-11-29 23:07:57 -0300

    testing

--- G03_Borrado.sql
+++ G03_Borrado.sql
@@ -1,27 +1,37 @@
-drop table if exists g03_categoria cascade;
-drop table if exists g03_comenta cascade;
-drop table if exists g03_comentario cascade;
-drop table if exists g03_juega cascade;
-drop table if exists g03_juego cascade;
-drop table if exists g03_nivel cascade;
-drop table if exists g03_recomendacion cascade;
-drop table if exists g03_tipo_usuario cascade;
-drop table if exists g03_usuario cascade;
-drop table if exists g03_voto cascade;
-drop function if exists FN_G03_FECHA_COMENTARIOS cascade;
-drop trigger if exists TR_G03_FECHA_COMENTARIOS cascade;
-drop function if exists FN_G03_UN_COMENTARIO_DIARIO cascade;
-drop trigger if exists TR_G03_UN_COMENTARIO_DIARIO cascade;
-drop function if exists FN_G03_RECOMENDACION_VOTADO cascade;
-drop trigger if exists TR_G03_RECOMENDACION_VOTADO cascade;
-drop function if exists FN_G03_COMENTAR_JUEGO CASCADE;
-drop trigger if exists TR_G03_COMENTAR_JUEGO cascade;
-drop function if exists FN_G03_AUDIT_COMENTA_COMENTARIO cascade;
-drop trigger if exists TR_G03_AUDIT_COMENTA_COMENTARIO cascade;
-drop function if exists FN_G03_PATRON_BUSQUEDA_APELLIDO cascade;
-drop view if exists VW_G03_COMENTARIOS_MES cascade;
-drop view if exists VW_G03_USUARIOS_COMENTADORES cascade;
-drop view if exists VW_G03_LOS_20_JUEGOS_MAS_PUNTUADOS cascade;
-drop view if exists VW_G03_LOS_10_JUEGOS_MAS_JUGADOS cascade;
+drop table if exists GR03
+_categoria cascade;
+drop table if exists GR03
+_comenta cascade;
+drop table if exists GR03
+_comentario cascade;
+drop table if exists GR03
+_juega cascade;
+drop table if exists GR03
+_juego cascade;
+drop table if exists GR03
+_nivel cascade;
+drop table if exists GR03
+_recomendacion cascade;
+drop table if exists GR03
+_tipo_usuario cascade;
+drop table if exists GR03
+_usuario cascade;
+drop table if exists GR03
+_voto cascade;
+drop function if exists TRFN_GR03_FECHA_COMENTARIOS cascade;
+drop trigger if exists TR_GR03_FECHA_COMENTARIOS cascade;
+drop function if exists TRFN_GR03_UN_COMENTARIO_DIARIO cascade;
+drop trigger if exists TR_GR03_UN_COMENTARIO_DIARIO cascade;
+drop function if exists TRFN_GR03_RECOMENDACION_VOTADO cascade;
+drop trigger if exists TR_GR03_RECOMENDACION_VOTADO cascade;
+drop function if exists TRFN_GR03_COMENTAR_JUEGO CASCADE;
+drop trigger if exists TR_GR03_COMENTAR_JUEGO cascade;
+drop function if exists TRFN_GR03_AUDIT_COMENTA_COMENTARIO cascade;
+drop trigger if exists TR_GR03_AUDIT_COMENTA_COMENTARIO cascade;
+drop function if exists FN_GR03_PATRON_BUSQUEDA_APELLIDO cascade;
+drop view if exists GR03_COMENTARIOS_MES cascade;
+drop view if exists GR03_USUARIOS_COMENTADORES cascade;
+drop view if exists GR03_LOS_20_JUEGOS_MAS_PUNTUADOS cascade;
+drop view if exists GR03_LOS_10_JUEGOS_MAS_JUGADOS cascade;
 
 
--- G03_Cambios.sql
+++ G03_Cambios.sql
@@ -4,110 +4,99 @@
    a la fecha del último comentario si este no es nulo */
 
 /*
-ALTER TABLE g03_comenta
+ALTER TABLE GR03_comenta
     ADD CONSTRAINT CK_FECHA_COMENTARIOS
         CHECK (NOT EXISTS(
                 SELECT 1
-                FROM g03_comenta
+                FROM GR03_comenta
                 WHERE fecha_primer_com > fecha_ultimo_com
             ));
 */
-
+--  INSERT INTO gr03_comenta (id_usuario, id_juego, fecha_primer_com, fecha_ultimo_com) values (1,9,'2020-01-11','2020-01-08');
 -- MODIFICADO PORQUE ESTABA MAL
-CREATE OR REPLACE FUNCTION FN_G03_FECHA_COMENTARIOS() RETURNS Trigger AS
+CREATE OR REPLACE FUNCTION TRFN_GR03_FECHA_COMENTARIOS() RETURNS Trigger AS
 $$
 DECLARE
-    fecha_primero G03_COMENTA.fecha_primer_com%type;
 BEGIN
-    SELECT fecha_primer_com
-    INTO fecha_primero
-    FROM G03_COMENTA
-    WHERE id_usuario = NEW.id_usuario
-      AND id_juego = NEW.id_juego
-    ORDER BY id_usuario, id_juego ASC
-    limit 1;
-    IF (fecha_primero > NEW.fecha_ultimo_com) THEN
+    IF (NEW.fecha_primer_com > NEW.fecha_ultimo_com AND NEW.fecha_ultimo_com IS NOT NULL) THEN
         RAISE EXCEPTION 'La fecha de su comentario % es anterior a la fecha del primer comentario %',
-            NEW.fecha_ultimo_com, fecha_primero;
+            NEW.fecha_ultimo_com, NEW.fecha_primer_com;
     END IF;
     RETURN NEW;
 END
 $$
     LANGUAGE 'plpgsql';
-
 /* este es un trigger de tabla */ /* Va en comentario o comenta????*/
-CREATE TRIGGER TR_G03_FECHA_COMENTARIOS
+CREATE TRIGGER TR_GR03_FECHA_COMENTARIOS
     BEFORE INSERT
-    ON G03_COMENTA
+    ON gr03_comenta
     FOR EACH ROW
-    WHEN ((SELECT fecha_ultimo_com FROM g03_comenta
-    WHERE g03_comenta.id_usuario= NEW.id_usuario AND g03_comenta.id_juego=NEW.id_juego) <> null)
-EXECUTE PROCEDURE FN_G03_FECHA_COMENTARIOS();
+EXECUTE PROCEDURE TRFN_GR03_FECHA_COMENTARIOS();
 
 
 /* b.	Cada usuario sólo puede comentar una vez al día cada juego */
 /*
-ALTER TABLE g03_comentario
+ALTER TABLE GR03_comentario
     ADD CONSTRAINT CK_UN_COMENTARIO_DIARIO
         CHECK (NOT EXISTS(
                 SELECT 1
-                FROM g03_comentario
+                FROM GR03_comentario
                 group by extract(day from fecha_comentario)
                 having COUNT(*)>1
             ));
 */
 
-CREATE OR REPLACE FUNCTION FN_G03_UN_COMENTARIO_DIARIO() RETURNS Trigger AS
+CREATE OR REPLACE FUNCTION TRFN_GR03_UN_COMENTARIO_DIARIO() RETURNS Trigger AS
 $$
 DECLARE
-    fecha_comentario_h G03_COMENTARIO.fecha_comentario%type;
+    fecha_comentario_h GR03_COMENTARIO.fecha_comentario%type;
 BEGIN
     SELECT fecha_comentario
     INTO fecha_comentario_h
-    FROM G03_COMENTARIO
+    FROM GR03_COMENTARIO
     WHERE id_juego = NEW.id_juego
       AND id_usuario = NEW.id_usuario
-      AND date(fecha_comentario) = date(NEW.fecha_comentario);
-    IF (fecha_comentario_h) THEN
+      AND fecha_comentario = NEW.fecha_comentario;
+    IF (fecha_comentario_h is not null) THEN
         RAISE EXCEPTION 'Ya comentaste hoy';
     END IF;
     RETURN NEW;
 END
 $$
     LANGUAGE 'plpgsql';
-
+-- INSERT INTO gr03_comentario (id_usuario, id_juego, id_comentario, fecha_comentario, comentario) VALUES (1,5,3,'2020-01-11', 'bla bla');
+-- INSERT INTO gr03_comentario (id_usuario, id_juego, id_comentario, fecha_comentario, comentario) VALUES (1,5,4,'2020-01-11', 'bla bla');
 /* este es un trigger de tabla */
-CREATE TRIGGER TR_G03_UN_COMENTARIO_DIARIO
+CREATE TRIGGER TR_GR03_UN_COMENTARIO_DIARIO
     BEFORE INSERT
-    ON G03_COMENTARIO
+    ON GR03_COMENTARIO
     FOR EACH ROW
-EXECUTE PROCEDURE FN_G03_UN_COMENTARIO_DIARIO();
+EXECUTE PROCEDURE TRFN_GR03_UN_COMENTARIO_DIARIO();
 
 /* c.	Un usuario no puede recomendar un juego si no ha votado previamente dicho juego */
 /*
-ALTER TABLE g03_recomendacion
+ALTER TABLE GR03_recomendacion
     ADD CONSTRAINT CK_RECOMENDACION_VOTADO
         CHECK (NOT EXISTS(
                 SELECT 1
-                FROM g03_recomendacion
+                FROM GR03_recomendacion
                 WHERE id_usuario, id_juego NOT IN (SELECT id_usuario, id_juego
-                                                   FROM g03_voto)
+                                                   FROM GR03_voto)
             ));
 */
-
-CREATE OR REPLACE FUNCTION FN_G03_RECOMENDACION_VOTADO() RETURNS Trigger AS
+-- INSERT INTO gr03_recomendacion (id_recomendacion, email_recomendado, id_usuario, id_juego) VALUES (1,'mail', 11,11);
+-- INSERT INTO gr03_voto (id_voto, valor_voto, id_usuario, id_juego) VALUES (350,4,11,11);
+CREATE OR REPLACE FUNCTION TRFN_GR03_RECOMENDACION_VOTADO() RETURNS Trigger AS
 $$
 DECLARE
-    id_user G03_RECOMENDACION.id_usuario%type;
-    id_game G03_RECOMENDACION.id_juego%type;
+    id_user GR03_RECOMENDACION.id_usuario%type;
 BEGIN
-    SELECT id_usuario, id_juego
-    into id_user, id_game
-    FROM G03_VOTO
+    SELECT id_usuario
+    into id_user
+    FROM GR03_VOTO
     WHERE id_usuario = NEW.id_usuario
       AND id_juego = NEW.id_juego;
-    IF (id_user <> NEW.id_usuario
-        AND id_game <> NEW.id_juego) THEN
+    IF (id_user is null) THEN
         RAISE EXCEPTION 'Solo podras recomendar juegos que hayas votado';
     END IF;
     RETURN NEW;
@@ -116,37 +105,39 @@ $$
     LANGUAGE 'plpgsql';
 -- modificado porque solo puedo recomendar juegos que SI haya votado
 /* este es una asercion (? */
-CREATE TRIGGER TR_G03_RECOMENDACION_VOTADO
+CREATE TRIGGER TR_GR03_RECOMENDACION_VOTADO
     BEFORE INSERT OR UPDATE of id_usuario,id_juego
-    ON G03_RECOMENDACION
+    ON GR03_RECOMENDACION
     FOR EACH ROW
-EXECUTE PROCEDURE FN_G03_RECOMENDACION_VOTADO();
+EXECUTE PROCEDURE TRFN_GR03_RECOMENDACION_VOTADO();
 
 /* d.	Un usuario no puede comentar un juego que no ha jugado */
-/* ALTER TABLE g03_recomendacion
+/* ALTER TABLE GR03_recomendacion
     ADD CONSTRAINT CK_COMENTAR_JUEGO
         CHECK (NOT EXISTS(
                 SELECT 1
-                FROM g03_comentario
+                FROM GR03_comentario
                 WHERE id_usuario, id_juego NOT IN (SELECT id_usuario, id_juego
-                                                   FROM g03_juega)
+                                                   FROM GR03_juega)
             ));
 
  */
-
-CREATE OR REPLACE FUNCTION FN_G03_COMENTAR_JUEGO() RETURNS Trigger AS
+-- INSERT INTO gr03_comenta (id_usuario, id_juego, fecha_primer_com, fecha_ultimo_com) VALUES (101,1,'2020-10-10', '2020-10-11');
+-- insert into gr03_comentario (id_usuario, id_juego, id_comentario, fecha_comentario, comentario) VALUES (101,1,1,'2020-11-1','coment');
+-- INSERT INTO gr03_juega (finalizado, id_usuario, id_juego) VALUES (true,101,1);
+CREATE OR REPLACE FUNCTION TRFN_GR03_COMENTAR_JUEGO() RETURNS Trigger AS
 $$
 DECLARE
-    id_user g03_comentario.id_usuario%type;
-    id_game g03_comentario.id_juego%type;
+    id_user GR03_comentario.id_usuario%type;
+    id_game GR03_comentario.id_juego%type;
 BEGIN
     SELECT id_usuario, id_juego
     into id_user, id_game
-    FROM G03_JUEGA
+    FROM GR03_JUEGA
     WHERE id_usuario = NEW.id_usuario
       AND id_juego = NEW.id_juego;
-    IF (id_user <> NEW.id_usuario
-        AND id_game <> NEW.id_juego) THEN
+    IF (id_user is null
+        AND id_game is null) THEN
         RAISE EXCEPTION 'Solo podras votar juegos que hayas jugado';
     END IF;
     RETURN NEW;
@@ -155,11 +146,11 @@ $$
     LANGUAGE 'plpgsql';
 
 /* este es una asercion (? */
-CREATE TRIGGER TR_G03_COMENTAR_JUEGO
+CREATE TRIGGER TR_GR03_COMENTAR_JUEGO
     BEFORE INSERT
-    ON g03_comentario
+    ON GR03_comentario
     FOR EACH ROW
-EXECUTE PROCEDURE FN_G03_COMENTAR_JUEGO();
+EXECUTE PROCEDURE TRFN_GR03_COMENTAR_JUEGO();
 
 -- C
 /*  1- Se debe mantener sincronizadas las tablas COMENTA y COMENTARIO en los siguientes aspectos:
@@ -167,77 +158,81 @@ La primera vez que se inserta un comentario de un usuario para un juego se debe 
     en ambas tablas, colocando la fecha del primer comentario y última fecha comentario en nulo.
 Los posteriores comentarios sólo deben modificar la fecha de último comentario e insertar en COMENTARIO
 */ /*creo xd*/
-
-CREATE OR REPLACE FUNCTION FN_G03_AUDIT_COMENTA_COMENTARIO()
+-- INSERT INTO GR03_USUARIO (id_usuario,nombre,apellido,email,id_tipo_usuario,password) VALUES (101,'Cairo','Curry','elit@elitafeugiat.co.uk',18,'PHJ15WBJ9PW');
+-- INSERT INTO gr03_comentario (id_usuario, id_juego, id_comentario, fecha_comentario, comentario) VALUES (101,1,50,NOW(),'comentario');
+-- INSERT INTO gr03_comentario (id_usuario, id_juego, id_comentario, fecha_comentario, comentario) VALUES (101,1,80,NOW(),'comentario');
+-- INSERT INTO gr03_juega (finalizado, id_usuario, id_juego) VALUES (true,101,1);
+CREATE OR REPLACE FUNCTION TRFN_GR03_AUDIT_COMENTA_COMENTARIO()
 RETURNS Trigger AS
 $$
 DECLARE
-    fecha_primer_coment  g03_comenta.fecha_primer_com%type;
+    fecha_primer_coment  GR03_comenta.fecha_primer_com%type;
 BEGIN
     SELECT fecha_primer_com into fecha_primer_coment
-    FROM g03_comenta
-    WHERE g03_comenta.id_usuario = NEW.id_usuario
-    AND g03_comenta.id_juego = NEW.id_juego;
-    IF NOT EXISTS(fecha_primer_coment) THEN
-        INSERT INTO g03_comenta (id_usuario, id_juego, fecha_primer_com, fecha_ultimo_com) VALUES (NEW.id_usuario,NEW.id_juego,NEW.fecha_comentario, null);
+    FROM GR03_comenta
+    WHERE GR03_comenta.id_usuario = NEW.id_usuario
+    AND GR03_comenta.id_juego = NEW.id_juego;
+    IF (fecha_primer_coment is null) THEN
+        INSERT INTO GR03_comenta (id_usuario, id_juego, fecha_primer_com, fecha_ultimo_com) VALUES (NEW.id_usuario,NEW.id_juego,NEW.fecha_comentario, null);
     ELSE
-        UPDATE g03_comenta SET fecha_ultimo_com = NEW.fecha_comentario WHERE id_juego = NEW.id_juego AND id_usuario = NEW.id_usuario;
+        UPDATE GR03_comenta SET fecha_ultimo_com = NEW.fecha_comentario WHERE id_juego = NEW.id_juego AND id_usuario = NEW.id_usuario;
     END IF;
+    RETURN NEW;
 END;
 $$
     LANGUAGE 'plpgsql';
 
-
-CREATE TRIGGER TR_G03_AUDIT_COMENTA_COMENTARIO
-    AFTER INSERT OR UPDATE OF id_usuario, id_juego, fecha_comentario
-    ON g03_comentario
+CREATE TRIGGER TR_GR03_AUDIT_COMENTA_COMENTARIO
+    BEFORE INSERT OR UPDATE OF id_usuario, id_juego, fecha_comentario
+    ON GR03_comentario
     FOR EACH ROW
-EXECUTE PROCEDURE FN_G03_AUDIT_COMENTA_COMENTARIO();
+EXECUTE PROCEDURE TRFN_GR03_AUDIT_COMENTA_COMENTARIO();
 
 /* 2- Dado un patrón de búsqueda devolver todos los datos de el o los usuarios junto con la cantidad de
    juegos que ha jugado y la cantidad de votos que ha realizado. */
 
-CREATE OR REPLACE FUNCTION FN_G03_PATRON_BUSQUEDA_APELLIDO(patron varchar)
+DROP FUNCTION FN_GR03_PATRON_BUSQUEDA_APELLIDO(patron varchar);
+CREATE OR REPLACE FUNCTION FN_GR03_PATRON_BUSQUEDA_APELLIDO(patron varchar)
 RETURNS TABLE (
-        id_usuario g03_usuario.id_usuario%type,
-        apellido g03_usuario.apellido%type,
-        nombre g03_usuario.nombre%type,
-        email g03_usuario.email%type,
-        id_tipo_usuario g03_usuario.id_tipo_usuario%type,
-        password g03_usuario.password%type,
+        id_usuarío GR03_usuario.id_usuario%type,
+        apellido GR03_usuario.apellido%type,
+        nombre GR03_usuario.nombre%type,
+        email GR03_usuario.email%type,
+        id_tipo_usuario GR03_usuario.id_tipo_usuario%type,
+        password GR03_usuario.password%type,
         cant_juegos_jugados INT,
-        cant_votos  INT
+        cant_votos INT
 )
 AS $$
 BEGIN
     RETURN QUERY SELECT
-       g03_usuario.id_usuario, apellido, nombre, email, id_tipo_usuario, password, coalesce(cant_juegos_jugados,0) as cant_juegos_jugados, coalesce(cant_votos,0) as cant_votos
+       u.id_usuario, u.apellido, u.nombre, u.email, u.id_tipo_usuario, u.password, coalesce(cant_juegos,0)::INTEGER as cant_juegos_jugados, coalesce(cant_v,0)::INTEGER as cant_votos
     FROM
-        g03_usuario left join (SELECT id_usuario, COUNT(*) as cant_juegos_jugados
-                        FROM g03_juega
-                        GROUP BY id_usuario) as juega on (g03_usuario.id_usuario = juega.id_usuario)
-            left join  (SELECT id_usuario, COUNT(*) as cant_votos
-                        FROM g03_voto
-                        GROUP BY id_usuario) as voto on (g03_usuario.id_usuario = voto.id_usuario)
+        GR03_usuario u left join (SELECT id_usuario, COUNT(*) as cant_juegos
+                        FROM GR03_juega
+                        GROUP BY id_usuario) as juega on (u.id_usuario = juega.id_usuario)
+            left join  (SELECT id_usuario, COUNT(*) as cant_v
+                        FROM GR03_voto
+                        GROUP BY id_usuario) as voto on (u.id_usuario = voto.id_usuario)
     WHERE
-        g03_usuario.apellido ILIKE '%'||patron||'%';
+        u.apellido ILIKE '%'||patron||'%';
 
 END; $$
 LANGUAGE 'plpgsql';
 --D
 /* COMENTARIOS_MES: Listar todos los comentarios realizados durante el último mes descartando aquellos
    juegos de la Categoría “Sin Categorías”. */
-CREATE VIEW VW_G03_COMENTARIOS_MES AS
+CREATE VIEW GR03_COMENTARIOS_MES AS
 SELECT comentario, fecha_comentario
-FROM G03_COMENTARIO
-WHERE G03_COMENTARIO.id_juego IN (SELECT id_juego
-                                  FROM g03_comenta
+FROM GR03_COMENTARIO
+WHERE GR03_COMENTARIO.id_juego IN (SELECT id_juego
+                                  FROM GR03_comenta
                                   WHERE id_juego IN (
                                       SELECT id_juego
-                                      FROM g03_juego
+                                      FROM GR03_juego
                                       WHERE id_categoria IN (
                                           SELECT id_categoria
-                                            FROM g03_categoria
+                                            FROM GR03_categoria
                                                 WHERE descripcion <> 'Sin Categorías'
                                           )
                                   ))
@@ -251,47 +246,41 @@ WHERE G03_COMENTARIO.id_juego IN (SELECT id_juego
 --teniendo en cuenta que sólo pueden comentar aquellos juegos que han jugado.
 -- ESTA IMPLEMENTADO EN trigger B sub d
 
-CREATE VIEW VW_G03_USUARIOS_COMENTADORES AS
+CREATE VIEW GR03_USUARIOS_COMENTADORES AS
 SELECT *
-FROM g03_usuario
-WHERE id_usuario IN (SELECT G03_COMENTA.id_usuario
-                     FROM g03_comenta
-                     WHERE G03_COMENTA.id_usuario IN (
+FROM GR03_usuario
+WHERE id_usuario IN (SELECT GR03_COMENTA.id_usuario
+                     FROM GR03_comenta
+                     WHERE GR03_COMENTA.id_usuario IN (
                          SELECT id_usuario
-                         FROM g03_comentario
+                         FROM GR03_comentario
                          WHERE fecha_comentario
                              BETWEEN NOW() - '1 year'::interval AND NOW()
-                             group by g03_comentario.id_usuario
-                         HAVING COUNT(id_juego) = (SELECT COUNT(id_juego) FROM G03_JUEGO)));
-CREATE VIEW USUARIOS_COMENTADORES AS
-SELECT *
-FROM g03_usuario
-WHERE id_usuario IN (SELECT id_usuario
-                     FROM g03_comenta
-                     WHERE fecha_ultimo_com >= NOW() - interval '1 year'
-                     GROUP BY id_usuario
-                     HAVING count(*) = (SELECT count(*)
-                                         FROM g03_juega));
+                             group by GR03_comentario.id_usuario
+                    HAVING COUNT(id_juego) = (SELECT COUNT(id_juego) FROM GR03_JUEGO)));
 /* LOS_20_JUEGOS_MAS_PUNTUADOS: Realizar el ranking de los 20 juegos mejor puntuados por los Usuarios.
    El ranking debe ser generado considerando el promedio del valor puntuado por los usuarios y que el
    juego hubiera sido calificado más de 5 veces. */
-
-CREATE VIEW VW_G03_LOS_20_JUEGOS_MAS_PUNTUADOS AS
-SELECT *
-FROM g03_juego
-WHERE id_juego IN (SELECT id_juego
-                   FROM g03_voto
-                   GROUP BY id_juego
+/* NO ES ACTUALIZABLE */
+CREATE VIEW GR03_LOS_20_JUEGOS_MAS_PUNTUADOS AS
+SELECT j.id_juego, j.nombre_juego, j.descripcion_juego, j.id_categoria FROM gr03_juego j join gr03_voto g03v on j.id_juego = g03v.id_juego
+                    GROUP BY j.id_juego
                    HAVING count(*) > 5
                    ORDER BY AVG(valor_voto) ASC
-                   LIMIT 20);
+                   LIMIT 20;
 /* LOS_10_JUEGOS_MAS_JUGADOS: Generar una vista con los 10 juegos más jugados. */
- /* NO ESTA FUNCIONANDO BIEN TRAE LAS ID USUARIOS DESORDENADAS */
-CREATE OR REPLACE VIEW VW_G03_LOS_10_JUEGOS_MAS_JUGADOS AS
+CREATE OR REPLACE VIEW GR03_LOS_10_JUEGOS_MAS_JUGADOS AS
+ /* ACTUALIZABLE NO ORDENADO AL MOSTRAR LOS 10 */
 SELECT *
-FROM g03_juego o
+FROM GR03_juego o
 WHERE o.id_juego IN (SELECT a.id_juego
-                   FROM g03_juega a
+                   FROM GR03_juega a
                    GROUP BY a.id_juego
                    ORDER BY COUNT(a.id_juego) DESC
                    LIMIT 10);
+/* NO ACTUALIZABLE, ORDERNADO AL MOSTRAR LOS 10*/
+-- SELECT o.id_juego, o.nombre_juego, o.descripcion_juego, o.id_categoria FROM gr03_juego o join gr03_juega J on o.id_juego = j.id_juego
+--                     GROUP BY o.id_juego
+--                     ORDER BY COUNT(j.id_juego) DESC
+--                     LIMIT 10;
+
